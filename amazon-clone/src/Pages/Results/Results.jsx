61a7569579cf21e797596412f4e074ea8240ba47
Datech2024 <dawitwelene@gmail.com>
2024-11-29 22:57:24 -0500

    Loading-functionality

--- amazon-clone/src/Pages/Results/Results.jsx
+++ amazon-clone/src/Pages/Results/Results.jsx
@@ -4,35 +4,43 @@ import LayOut from "../../Components/LayOut/LayOut";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import ProductCard from "../../Components/Product/ProductCard";
-import { productUrl } from "../../Api/EndPoints.jsX";
-
+import { productUrl } from "../../Api/EndPoints";
+import Loader from "../../Components/Loader/Loader";
 
 function Results() {
 	const [results, setResults] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const { categoryName } = useParams();
 	useEffect(() => {
+		setIsLoading(true);
 		axios
 			.get(`${productUrl}/products/category/${categoryName}`)
 			.then((res) => {
 				setResults(res.data);
+				setIsLoading(false);
 			})
 			.catch((err) => {
 				console.log(err);
+				setIsLoading(false);
 			});
 	}, []);
 
 	return (
 		<LayOut>
-			<section>
-				<h1 style={{ padding: "30px" }}>Results</h1>
-				<p>Category / {categoryName}</p>
-				<hr />
-				<div className={classes.products_container}>
-					{results?.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
-				</div>
-			</section>
+			{isLoading? (
+				<Loader />
+			) : (
+				<section>
+					<h1 style={{ padding: "30px" }}>Results</h1>
+					<p>Category / {categoryName}</p>
+					<hr />
+					<div className={classes.products_container}>
+						{results?.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						))}
+					</div>
+				</section>
+			)}
 		</LayOut>
 	);
 }
