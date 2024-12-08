c22a313a4bbea71991599557b27268c15deaa6f0
Datech2024 <dawitwelene@gmail.com>
2024-12-08 17:19:32 -0500

    Backend-deployment-using-cff

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://127.0.0.1:5001/clone-a98dd/us-central1/api",
+	// baseURL: "http://127.0.0.1:5001/clone-a98dd/us-central1/api",
+	baseURL: "https://us-central1-clone-a98dd.cloudfunctions.net/api",
 });
 
 export { axiosInstance };
